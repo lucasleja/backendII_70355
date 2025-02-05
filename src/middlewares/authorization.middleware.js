ae16368006830855ba32104350ebbdc25270ce6b
lucasleja <lejarragalucas@gmail.com>
2025-02-05 19:53:15 -0300

    current solucionado

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,8 +1,21 @@
 
-export const authorization = (roles) => {
+/* export const authorization = (roles) => {
   return async (req, res, next) => {
       if(!req.user) return res.status(401).json({ error: "No autorizado"});
       if(req.user.roles !== roles) res.status(403).json({ error: "Rol inválido"});
       next();
   }
-}
+} */
+
+
+export const authorization = (roles) => {
+
+  return async (req, res, next) => {
+  
+  if (!req.user) { return res.status(401).json({ error: "No autorizado" }) };
+  
+  if (req.user.roles !== roles) { return res.status(403).json({ error: "Rol inválido" }) };
+  
+  next();
+  }
+  }
